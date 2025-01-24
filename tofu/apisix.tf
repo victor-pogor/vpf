3ea79383c987ef7b536252d98c7d3ea6f2ca4a57
Victor Pogor <victor.pogor@outlook.com>
2025-01-25 01:36:02 +1000

    feat(tofu): moved apisix resources in different namespaces

--- tofu/apisix.tf
+++ tofu/apisix.tf
@@ -1,6 +1,18 @@
 resource "kubernetes_namespace_v1" "apisix_namespace" {
   metadata {
-    name = "ingress-apisix"
+    name = "apisix"
+  }
+}
+
+resource "kubernetes_namespace_v1" "apisix_dashboard_namespace" {
+  metadata {
+    name = "apisix-dashboard"
+  }
+}
+
+resource "kubernetes_namespace_v1" "apisix_ingress_namespace" {
+  metadata {
+    name = "apisix-ingress"
   }
 }
 
@@ -14,14 +26,26 @@ resource "helm_release" "apisix" {
 
 resource "helm_release" "apisix_dashboard" {
   name       = "apisix-dashboard"
-  namespace  = kubernetes_namespace_v1.apisix_namespace.metadata[0].name
+  namespace  = kubernetes_namespace_v1.apisix_dashboard_namespace.metadata[0].name
   repository = "https://charts.apiseven.com"
   chart      = "apisix-dashboard"
   version    = "0.8.2"
-  values = [templatefile("${path.module}/config/apisix_dashboard_values.yaml", {
-    host            = var.apisix_dashboard.host
-    selfsigned_name = var.cert_manager.cluster_issuers.selfsigned.name
-    username        = var.apisix_dashboard.username
-    password        = var.apisix_dashboard.password
+  values = [templatefile("${path.module}/config/apisix_dashboard.yaml", {
+    host                     = var.apisix_dashboard.host
+    selfsigned_name          = var.cert_manager.cluster_issuers.selfsigned.name
+    username                 = var.apisix_dashboard.username
+    password                 = var.apisix_dashboard.password
+    apisix_service_namespace = kubernetes_namespace_v1.apisix_namespace.metadata[0].name
+  })]
+}
+
+resource "helm_release" "apisix_ingress_controller" {
+  name       = "apisix-ingress-controller"
+  namespace  = kubernetes_namespace_v1.apisix_ingress_namespace.metadata[0].name
+  repository = "https://charts.apiseven.com"
+  chart      = "apisix-ingress-controller"
+  version    = "0.14.0"
+  values = [templatefile("${path.module}/config/apisix_ingress_controller.yaml", {
+    apisix_service_namespace = kubernetes_namespace_v1.apisix_namespace.metadata[0].name
   })]
 }
